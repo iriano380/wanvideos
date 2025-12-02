32d1e5885ff7f65e032e21457b87c7392e72846b
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-12-02 18:08:25 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <nav className="flex items-center justify-between px-8 py-2 backdrop-blur-lg bg-white/70 shadow shadow-zinc-200 rounded-b-2xl w-full mx-auto fixed z-50 -translate-x-1/2 left-1/2">
       <div className="flex items-center space-x-2 text-2xl font-bold text-zinc-900">
         <div className="w-8 h-8 bg-linear-to-r from-pink-400 to-purple-500 rounded-full hover:animate-spin"></div>
-        <span>Xnsta</span>
+        <span>Ins</span>
       </div>
       <div className="hidden md:flex items-center space-x-8 text-zinc-700">
         <a
@@ -16,7 +16,7 @@ export default function Header() {
         </a>
       </div>
       <button className="bg-rose-600 text-white px-6 py-2 rounded-full hover:bg-rose-700 transition-all flex items-center space-x-2">
-        <span>Follow</span>
+        <span>Seguir</span>
         <svg
           className="w-4 h-4 hidden sm:block"
           fill="none"
